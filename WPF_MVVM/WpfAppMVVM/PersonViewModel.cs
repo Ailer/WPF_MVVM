b042ead42d2a0acf0070661df161eadcf6312cd0
Ailer39 <NilsKunkel@web.de>
2018-01-23 11:12:16 +0100

    WPFMVVMApp

--- WPF_MVVM/WpfAppMVVM/PersonViewModel.cs
+++ WPF_MVVM/WpfAppMVVM/PersonViewModel.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WPF.Common.ViewModels;
 
 namespace WpfAppMVVM
 {
-    class PersonViewModel
+    class PersonViewModel : ViewModelBase
     {
         private Person pers = new Person();
         string txtPersonName;
@@ -14,7 +15,16 @@ namespace WpfAppMVVM
         bool isMarried;
 
         public string TxtPersonName { get => txtPersonName; set => txtPersonName = value; }
-        public int TxtPersonAge { get => txtPersonAge; set => txtPersonAge = value; }
+        public int TxtPersonAge
+        {
+            get => txtPersonAge; set
+            {
+                pers.Age = value;
+                txtPersonAge = value;
+                base.OnPropertyChanged("TxtPersonAge");
+                base.OnPropertyChanged("lAgeColor");
+            }
+        }
         public bool IsMarried { get => isMarried; set => isMarried = value; }
 
         public string lAgeColor //String fuer Wiederverwendbarkeit, nicht UI-spezifisch
